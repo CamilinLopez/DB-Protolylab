031a122ee3d6861365d92da90a1c7dc679c41463
CamilinLopez <camilollc98@gmail.com>
2024-01-22 20:50:14 -0500

    callbackredirect

--- src/middlewares/google.js
+++ src/middlewares/google.js
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback",
+      callbackURL: "https://protolylab.onrender.com/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       try {
