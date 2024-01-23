9dee8a8cc993e6e5045b4791f47ea34a6545f915
CamilinLopez <camilollc98@gmail.com>
2024-01-22 20:12:54 -0500

    deploy postgre render

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, EXTERNAl_DATABASE_URL } =
+  process.env;
 const modelProject = require("./models/Projects");
 const modelUser = require("./models/Users");
-const {Pool} = require("pg")
+const { Pool } = require("pg");
 
-
-const database = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  { logging: false, native: false }
-);
+// const database = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//   { logging: false, native: false }
+// );
+const database = new Sequelize(EXTERNAl_DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  native: false,
+});
 
 modelProject(database);
 modelUser(database);
