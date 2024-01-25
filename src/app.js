0255976e3eea644e935a54c2820aea893ff7f4df
CamilinLopez <camilollc98@gmail.com>
2024-01-25 13:41:26 -0500

    fallo cookie 8

--- src/app.js
+++ src/app.js
@@ -27,17 +27,17 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "300mb" }));
 server.use(bodyParser.json({ limit: "300mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 
-//   next();
-// });
+  next();
+});
 server.use(passport.initialize());
 
 server.use("/", routes);
