87c8462143194c90d15f1623112920b10712e382
CamilinLopez <camilollc98@gmail.com>
2024-01-26 12:36:48 -0500

    solve problem whe verifying user 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 const { Router } = require("express");
 const projectRouter = require("./projectRouter");
 const { authRouter } = require("../middlewares/google");
-const tokenRouter = require("./tokenRouter");
 const userRouter = require("./userRouter");
 
 const router = Router();
 
 router.use("/project", projectRouter);
 router.use("/auth/google", authRouter);
-router.use("/verify", tokenRouter);
 router.use("/user", userRouter);
 
 module.exports = router;
