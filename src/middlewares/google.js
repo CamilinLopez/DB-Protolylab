529c6a756eb94458b35ff360a3f2f9684eca2156
CamilinLopez <camilollc98@gmail.com>
2024-02-02 13:44:05 -0500

    config cookies 12

--- src/middlewares/google.js
+++ src/middlewares/google.js
@@ -90,23 +90,17 @@ authRouter.get(
       res.cookie("pkid", req.user.id, {
         secure: true,
         sameSite: "none",
-        domain: ".onrender.com",
         httpOnly: false,
-        path: "/",
       });
       res.cookie("token", token, {
         secure: true,
         sameSite: "none",
-        domain: ".onrender.com",
         httpOnly: false,
-        path: "/",
       });
       res.cookie("isadmin", data.isadmin, {
         secure: true,
         sameSite: "none",
-        domain: ".onrender.com",
         httpOnly: false,
-        path: "/",
       });
 
       res.redirect(`https://localhost:3000/dashboard`);
